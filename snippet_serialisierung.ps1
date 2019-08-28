d1d507acb94149b02c225ccf32a5f745f1598534
JoeVice <DanielWill@users.noreply.github.com>
2019-08-28 15:31:26 +0200

    JSON / XML Import + Export

---
+++ snippet_serialisierung.ps1
@@ -0,0 +1,18 @@
+﻿$WhatIfPreference = 0
+
+# JSON-File einlesen und in ein Objekt speichern mit ConvertFrom-Json (erwartet JSON-String)
+
+# $uk8slog01 = Get-Content "path/to/file.json" | ConvertFrom-Json
+
+# JSON-Datei erzeugen mit ConvertTo-Json (erzeugt JSON-String)
+
+$datumJson = Get-Date | Select-Object -Property * | ConvertTo-Json | Out-File "C:\users\dw2\Desktop\datum.json"
+
+# XML-Datei erzeugen mit ConvertTo-XML (erzeugt XML-Objekt)
+$datumXml = Get-Date | Select-Object -Property * | ConvertTo-Xml
+
+# Eigenschaft "InnerXML" angeben um kein XML-Objekt zu schreiben sondern das eigentliche XML als String
+$datumXml.InnerXml | Out-File "C:\users\dw2\Desktop\datum.xml"
+
+# XML Importieren
+$importedXml = Get-Content "C:\users\dw2\Desktop\datum.xml" | ConvertTo-XML
