df5131677242d47f120fd8d9910236b1a7b4a4fd
JoeVice <DanielWill@users.noreply.github.com>
2019-08-28 11:28:15 +0200

    Create snippet_Dienste.ps1
    Ausführung von Commandlets / DienstObjekte holen, auslesen, operieren

---
+++ snippet_Dienste.ps1
@@ -0,0 +1,29 @@
+﻿# Init
+$wuauserv = Get-Service -Name wuauserv
+$diensteListe = Get-Service
+
+# Dienste
+Get-Service -Name $wuauserv.Name | Stop-Service
+
+
+
+
+if((Get-Service -Name wuauserv).Status -eq "Stopped") {
+    Start-Service -Name wuauserv
+} else {
+    Stop-Service -Name wuauserv
+}
+
+
+if($wuauserv.Status -ne "Running") {
+    # Start-Methode direkt am ServiceController-Objekt
+    $wuauserv.Start() # Dienst starten wenn gestoppt
+    $wuauserv.Refresh() # Status aktualisieren
+    $wuauserv.Status() # Status ausgeben 
+
+    # Start über Commandlet
+    Start-Service -Name $wuauserv.Name
+}
+
+$diensteListe | % { Write-Host "Dienst $($_.Name) hat Status $($_.Status)" }
+
