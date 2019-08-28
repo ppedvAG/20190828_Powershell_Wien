d4d6af3f5b03da999f46a75bbcf80afbab9dd4bb
JoeVice <DanielWill@users.noreply.github.com>
2019-08-28 10:48:24 +0200

    Update snippet_Get-Values-of-allProperties.ps1
    Kürzere Variante

--- snippet_Get-Values-of-allProperties.ps1
+++ snippet_Get-Values-of-allProperties.ps1
@@ -1,2 +1,5 @@
 ﻿# Werte aller Eigenschaften eines Objektes holen
 (Get-VMHost | gm).Name | ForEach-Object { Write-Host "Prop: $($_) | Wert: " (Get-VMHost)."$($_)" }
+
+# Kürzere Variante
+Write-Host (Get-VMHost | gm).Name | Select -ExpandProperty $_
